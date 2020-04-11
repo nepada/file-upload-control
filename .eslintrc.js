ca51ea2b99ea51243e831c90e2a8d07dd2d2f0fe
Petr Morávek <petr@pada.cz>
2020-04-11 19:25:08 +0200

    CI skeleton

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es6': true,
+    },
+    'parserOptions': {
+        'ecmaVersion': 2018,
+        'sourceType': 'module',
+    },
+    'plugins': [
+        'import',
+    ],
+    'extends': [
+        '@nepada',
+    ],
+};
